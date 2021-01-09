66ab2af23047d36452a78117b477793b7fb01b88
felipdsanta <felipe.carro3@outlook.com>
2021-01-09 16:04:17 -0300

    Usando padrao DTO para retornar usuarios

--- src/main/java/com/felps/mongo/resource/UserResource.java
+++ src/main/java/com/felps/mongo/resource/UserResource.java
@@ -1,6 +1,7 @@
 package com.felps.mongo.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.felps.mongo.domain.User;
+import com.felps.mongo.dto.UserDTO;
 import com.felps.mongo.services.UserService;
 
 @RestController
@@ -20,9 +22,10 @@ public class UserResource {
 	private UserService service;
 	
 		@RequestMapping(method=RequestMethod.GET)
-		public ResponseEntity<List<User>> findAll(){
+		public ResponseEntity<List<UserDTO>> findAll(){
 			List<User> list = service.findAll();
-			return ResponseEntity.ok().body(list);
+			List<UserDTO> listDto =list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+			return ResponseEntity.ok().body(listDto);
 		}
 		
 	}
