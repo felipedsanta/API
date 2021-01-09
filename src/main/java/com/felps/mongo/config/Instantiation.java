6b6ac05eba486c91ef3ba27c71fd917907b90262
felipdsanta <felipe.carro3@outlook.com>
2021-01-09 18:48:15 -0300

    comentarios nos posts adcionado

--- src/main/java/com/felps/mongo/config/Instantiation.java
+++ src/main/java/com/felps/mongo/config/Instantiation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.felps.mongo.domain.Post;
 import com.felps.mongo.domain.User;
 import com.felps.mongo.dto.AuthorDTO;
+import com.felps.mongo.dto.CommentDTO;
 import com.felps.mongo.repository.PostRepository;
 import com.felps.mongo.repository.UserRepository;
 
@@ -42,7 +43,14 @@ public class Instantiation implements CommandLineRunner {
 		Post post1 = new Post(null, sdf.parse("21/04/2020"), "Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/04/2020"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
-		userRepository.saveAll(Arrays.asList(maria, alex, bob));postRepository.saveAll(Arrays.asList(post1, post2));
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/04/2020"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", sdf.parse("22/04/2020"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/04/2020"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1,c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1,post2));
 		userRepository.save(maria);
